61602fecd1845d950418def214a4bd37ff104296
josch <joscha.maier@live.de>
2021-12-21 13:00:44 +0100

    fixed import

--- src/MinimalEmail.java
+++ src/MinimalEmail.java
@@ -1,16 +1,13 @@
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+
 
 public class MinimalEmail extends Application {
     public void start(Stage stage) {
